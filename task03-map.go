406ea70aaa89fe2a9e6ea143e119d952689add4e
Peter Vereshagin <peter@vereshagin.org>
2022-05-06 07:53:59 +0000

    feat: :white_check_mark: implementation

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,35 @@
 package homework
 
+import (
+	"sort"
+)
+
+func getSortMapKeys(input map[int]string) (result []int) {
+	keys := make([]int, 0, len(input))
+
+	for k := range input {
+		keys = append(keys, k)
+	}
+
+	sort.Ints(keys)
+	return keys
+}
+
+func getSortMapValues(input map[int]string, keys []int) (values []string) {
+	values = make([]string, 0, len(input))
+
+	for sKey := range keys {
+		k := keys[sKey]
+		values = append(values, input[k])
+	}
+
+	return values
+}
+
 func sortMapValues(input map[int]string) (result []string) {
 	//Place your code here
-	return
+	keys := getSortMapKeys(input)
+	values := getSortMapValues(input, keys)
+
+	return values
 }
