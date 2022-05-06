c9f9e61e07d922ddb15e5b9535219a919b447bc1
Peter Vereshagin <peter@vereshagin.org>
2022-05-06 07:07:01 +0000

    feat: :white_check_mark: implementation

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,16 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
+
 	//Place your code here
-	return
+	result = make([]int64, len(input))
+	copy(result, input)
+
+	iLimit := len(input) / 2
+	for i := 0; i < iLimit; i++ {
+		idx := len(input) - 1 - i
+		result[i], result[idx] = result[idx], result[i]
+	}
+
+	return result
 }
